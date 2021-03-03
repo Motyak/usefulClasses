248168723d99841cfc4e367dbfcab85de6b0c794
Tommy Anfosso <anfossotommy@hotmail.fr>
2021-03-03 03:29:30 +0100

    TextFile OK

--- Mk/TextFile.java
+++ Mk/TextFile.java
@@ -4,48 +4,47 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.LineNumberReader;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
 /**
+ * Static class for reading from / writing to text files
  * @author Motyak
  */
 public class TextFile {
-	
+
 	public static void main(String[] args) throws Exception {
 		String userDir = System.getProperty("user.dir") + File.separator;
-		String content = new String("line1\nline2\nline3");
-		
+		String content = new String("1\n2\n3\n4\n5\n6\n7\n8\n9\n10\n11\n12");
+
 		System.out.println("creating file1 with the content of str..");
-		TextFile.stringToFile(content, userDir + "file1", false);
-		
-		System.out.println("renaming file1 into file2.. (and overwriting if the file already exists)");
-		TextFile.rename(userDir, "file1", "file2", true);
-		
+		TextFile.write(content, userDir + "file1");
+
+		System.out.println("renaming file1 into file2..");
+		TextFile.renameAndOverwrite(userDir, "file1", "file2");
+
 		System.out.println("reading and printing file2 content..");
-		System.out.println(TextFile.fileToString(userDir + "file2"));
-		
-		System.out.println("reading only the first and last line of file2..");
-		System.out.println(TextFile.overview(userDir + "file2", 1));
-		
+		System.out.println(TextFile.read(userDir + "file2"));
+
+		System.out.println("printing an overview of file2 (max 10 lines)..");
+		System.out.println(TextFile.overview(userDir + "file2"));
+
 		System.out.println("deleting file2..");
 		new File(userDir + "file2").delete();
 	}
 
 	/**
-	 * Read a file and return its content as a String
+	 * Read file content
 	 * @param filePath the absolute path of the targeted file
 	 * @return the content of the targeted file as a String
 	 * @throws IOException if there's a problem finding/reading the file
 	 */
-	public static String fileToString(String filePath) throws IOException {
+	public static String read(String filePath) throws IOException {
 		InputStream is = new FileInputStream(filePath);
 		BufferedReader buf = new BufferedReader(new InputStreamReader(is));
 		String line = buf.readLine();
@@ -55,78 +54,103 @@ public class TextFile {
 		   sb.append(line).append("\n");
 		   line = buf.readLine();
 		}
-		
+
 		buf.close();
 		sb.setLength(sb.length() - 1);	//Gets rid of the last line break to keep the exact same content
-		
+
 		return sb.toString();
 	}
-	
+
 	/**
-	 * Create a file and write a String in it
+	 * Write stringable to file (overwrite if exists, otherwise create it)
 	 * @param stringable the content to write into the file, will use 'toString()' implementation
 	 * @param filePath the absolute path of the file to create
 	 * @throws IOException if there's a problem finding/writing the file
 	 */
-	public static void stringToFile(Object stringable, String filePath, boolean append) throws IOException {
-		String str = stringable.toString();
+	public static void write(Object stringable, String filePath) throws IOException {
+		TextFile.write(stringable.toString(), filePath, false);
+	}
+
+	/**
+	 * Append stringable to file (create the file if not exists)
+	 * @param stringable the content to write into the file, will use 'toString()' implementation
+	 * @param filePath the absolute path of the file to create
+	 * @throws IOException if there's a problem finding/writing the file
+	 */
+	public static void append(Object stringable, String filePath) throws IOException {
+		TextFile.write(stringable.toString(), filePath, true);
+	}
+
+	private static void write(String str, String filePath, boolean append) throws IOException {
 		BufferedWriter writer = new BufferedWriter(new FileWriter(filePath, append));
 		writer.write(str);
 	    writer.close();
 	}
-	
+
 	/**
 	 * Read a file and return an overview of it
 	 * @param filePath the absolute path of the targeted file
-	 * @param nbOfLines the nb of first and last lines to return
-	 * @return an overview of the targeted file (certain number of lines)
+	 * @return an overview of the targeted file
 	 * @throws IOException if there's a problem finding/reading the file
 	 */
-	public static String overview(String filePath, int nbOfLines) throws IOException {
+	public static String overview(String filePath) throws IOException {
 		long totalNbOfLines = TextFile.nbOfLines(filePath);
-		
-		if(totalNbOfLines <= 2*nbOfLines)
-			return TextFile.fileToString(filePath);
-		
-		InputStream is = new FileInputStream(filePath);
-		BufferedReader buf = new BufferedReader(new InputStreamReader(is));
-		StringBuilder sb = new StringBuilder();
-		
-		LineNumberReader reader = new LineNumberReader(new FileReader(filePath));
-		for(int i = 1 ; i <= nbOfLines ; ++i)
-			sb.append(buf.readLine()).append("\n");
-		sb.append("...\n");
-		reader.skip(totalNbOfLines - nbOfLines);
-		for(int i = 1 ; i <= nbOfLines ; ++i)
-			sb.append(buf.readLine()).append("\n");
-		
-		buf.close();
-		reader.close();
-		sb.setLength(sb.length() - 1);
-		
-		return sb.toString();
+
+		if(totalNbOfLines <= 5)
+			return TextFile.read(filePath);
+		else {
+			InputStream is = new FileInputStream(filePath);
+			BufferedReader buf = new BufferedReader(new InputStreamReader(is));
+			StringBuilder sb = new StringBuilder();
+			for(int i = 1 ; i <= 5 ; ++i)
+					sb.append(buf.readLine()).append("\n");
+			sb.append("...\n");
+			for(int i = 1 ; i <= totalNbOfLines - 10 ; ++i)
+				buf.readLine();
+			for(int i = 1 ; i <= 5 ; ++i)
+				sb.append(buf.readLine()).append("\n");
+
+			buf.close();
+			sb.setLength(sb.length() - 1);
+
+			return sb.toString();
+		}
 	}
-	
+
 	/**
-	 * Rename an existing file
+	 * Rename a file
 	 * @param dirPath the directory containing the file to rename
-	 * @param oldFileName the name of the file to rename
-	 * @param newFileName the future name of the file
-	 * @param overwrite indicates if you wish to overwrite if there's already a file with the name you want
+	 * @param fileName the name of the file to rename
+	 * @param newFileName the new file name
 	 * @throws Exception if there's a problem finding/renaming the file
 	 */
-	public static void rename(String dirPath, String fileName, String newFileName, boolean overwrite) throws Exception {
+	public static void rename(String dirPath, String fileName, String newFileName) throws Exception {
+		TextFile.rename(dirPath, fileName, newFileName, false);
+	}
+
+	/**
+	 * Rename a file, overwrite if a file is already named as new file name
+	 * @param dirPath the directory containing the file to rename
+	 * @param fileName the name of the file to rename
+	 * @param newFileName the new file name
+	 * @throws Exception if there's a problem finding/renaming the file
+	 */
+	public static void renameAndOverwrite(String dirPath, String fileName, String newFileName) throws Exception {
+		TextFile.rename(dirPath, fileName, newFileName, true);
+	}
+
+	private static void rename(String dirPath, String fileName, String newFileName, boolean overwrite) throws Exception {
 		Path source = Paths.get(dirPath + fileName);
-		File file=new File(dirPath + newFileName);
+		File file = new File(dirPath + newFileName);
 		if(file.exists()) {
 			if(overwrite)
 				file.delete();
 			else
 				throw new Exception("Cannot rename : the name of the file already exists.");
 		}
-		Files.move(source,source.resolveSibling(newFileName));
+		Files.move(source, source.resolveSibling(newFileName));
 	}
-	
+
 	private static long nbOfLines(String filePath) throws IOException {
 		return Files.lines(Paths.get(filePath)).count();
 	}
